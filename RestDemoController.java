4ed8d2aa38a17972aa57b250bb93a17160fe1f27
Akhilesh Soni <49095481+akhilswin@users.noreply.github.com>
2024-09-26 07:03:36 +0530

    Add files via upload

---
+++ RestDemoController.java
@@ -0,0 +1,13 @@
+package com.restexample.restdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestDemoController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello Rest API New !!!";
+	}
+}
